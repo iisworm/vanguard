79be3f2a26bb1d2405ec99e901730919286dd3dc
Aeswere <20750227+Aeswere@users.noreply.github.com>
2021-02-11 14:39:28 -0500

    add ecosystem (#2)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = [{
   script: 'bot.js',
   name: 'Vanguard',
-  error_file: '../pm2 logs/error.log',
-  out_file: '../pm2 logs/out.log',
+  error_file: '../logs/error.log',
+  out_file: '../logs/out.log',
   time: true,
   watch: true,
   ignore_watch: 'node_modules',
